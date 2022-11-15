a34ee8f1a695128d632563541b9669a59314eadb
allakoval <2448572@gmail.com>
2022-11-15 14:22:27 +0500

    32

--- task31/Program.cs
+++ task31/Program.cs
@@ -5,27 +5,27 @@
 // положительных чисел равна 29, сумма отрицательных равна
 // // -20
 
-int[] CreateArraeRndInt(int size, int min, int max)
-{
-    int[] array = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = rnd.Next(min, max + 1);
-    }
-    return array;
-}
+// int[] CreateArraeRndInt(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.Next(min, max + 1);
+//     }
+//     return array;
+// }
 
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
-        else Console.Write($"{array[i]}");
-    }
-    Console.WriteLine("]");
-}
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine("]");
+// }
 
 // int GetSumPositivNegativElem(int[] array)
 // {
